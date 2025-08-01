e138ea4e25926efa0e2e496f82d0cd91ab079bbb
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-01 16:48:47 -0300

    feat: creatr countdown component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDoown } from './components/CountDoown';
 function App() {
   return (
     <>
@@ -12,6 +13,9 @@ function App() {
 
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDoown />
       </Container>
     </>
   );
---
+++ src/components/CountDoown/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.css';
+
+export function CountDoown() {
+  return <div className={styles.container}>00:00</div>;
+}
