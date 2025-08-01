9fe141c4e9d084723a8b2d229826036f778f9f54
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-01 15:42:04 -0300

    chore: folder organize

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <section>LOGO</section>
+      </Container>
+
+      <Container>
+        <section>MENU</section>
+      </Container>
     </>
   );
 }
