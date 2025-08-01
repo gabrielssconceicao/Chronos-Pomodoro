2d09c25b82b912c986cdd630f8c0eb3a8670db43
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-01 16:42:12 -0300

    feat: Logo and Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 function App() {
   return (
     <>
       <Container>
-        <section>LOGO</section>
+        <Logo />
       </Container>
 
       <Container>
-        <section>MENU</section>
+        <Menu />
       </Container>
     </>
   );
