c55666a7008f7e88fadd4ff4bc8ab40fbc2443c2
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-28 11:47:36 -0300

    feat: add count down using worker

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from 'react';
 import { intialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './task-reducer';
+import { TimerWorkerManager } from '../../workers/timer-worker-manager';
 
 interface TaskProviderProps {
   children: React.ReactNode;
@@ -10,9 +11,26 @@ interface TaskProviderProps {
 export function TaskContextProvider({ children }: TaskProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, intialTaskState);
 
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((e) => {
+    //e.data = seconds
+    const countDownSeconds = e.data;
+    console.log(countDownSeconds);
+
+    if (countDownSeconds <= 0) {
+      console.log('Worker Completed: terminado por tempo esgotado');
+      worker.terminate();
+    }
+  });
+
   useEffect(() => {
-    console.log(state);
-  }, [state]);
+    if (!state.activeTask) {
+      worker.terminate();
+      console.log('Worker: terminado por falta de active task');
+    }
+    worker.postMessage(state);
+  }, [worker, state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
--- src/workers/timer-worker.js
+++ src/workers/timer-worker.js
@@ -1,9 +1,30 @@
 // self.onmessage = is a built-in function that is called when a message is received from the main thread
-self.onmessage = function (event) {
-  //message from main thread
-  console.log('Worker: Message received from main script', event.data);
 
-  self.postMessage('Olá para voce tbm!');
+let isRunning = false;
+self.onmessage = function (event) {
+  if (isRunning) return;
+  isRunning = true;
+  //message from main thread
+  const state = event.data;
+  const { activeTask, secondsRemaining } = state;
+
+  const endDate = activeTask.startDate + secondsRemaining * 1000;
+
+  console.log(new Date(endDate));
+
+  const now = Date.now();
+
+  let countDownSeconds = Math.ceil((endDate - now) / 1000);
+  function tick() {
+    self.postMessage(countDownSeconds);
+    const now = Date.now();
+    countDownSeconds = Math.floor((endDate - now) / 1000);
+
+    // it will run again in 1 second
+    setTimeout(tick, 1000);
+  }
+
+  tick();
 };
 
 // Worker: it will run in a separate thread and will not block the main thread
