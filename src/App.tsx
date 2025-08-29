cb8475bb624d664bf1723cb4abdb8dee33ae2ad1
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-29 10:53:03 -0300

    feat: add router component and scroll page to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Home } from './pages/Home';
+
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessagesContainer } from './contexts/MessagesContainer';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { NotFound } from './pages/NotFound';
-import { About } from './pages/About';
+import { MainRouter } from './Routers';
+
 function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro/' element={<About />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
