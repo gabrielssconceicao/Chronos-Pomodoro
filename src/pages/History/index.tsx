7cb7eee616a966234bf28a772cd8f52a1c4ed0d6
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-29 17:31:39 -0300

    feat: add dict to get type

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -38,15 +38,22 @@ export function History() {
             </thead>
 
             <tbody>
-              {state.tasks.map((task) => (
-                <tr key={task.id}>
-                  <td>{task.name}</td>
-                  <td>{task.duration}min</td>
-                  <td>{formatDate(task.startDate)}</td>
-                  <td>{getTaskStatus(task, state.activeTask)}</td>
-                  <td>{task.type}</td>
-                </tr>
-              ))}
+              {state.tasks.map((task) => {
+                const taskTypeDict = {
+                  workTime: 'Foco',
+                  shortBreakTime: 'Descanso curto',
+                  longBreakTime: 'Descanso longo',
+                };
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDict[task.type]}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
