fa828e5e8cdcd21e32cd84e1f91570effcd7e447
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-30 15:30:18 -0300

    feat: validating setting form fields

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,11 +1,50 @@
+import { useRef } from 'react';
 import { SaveIcon } from 'lucide-react';
 import { Container } from '../../components/Container';
 import { DefaultButton } from '../../components/DefaultButton';
 import { DefaultInput } from '../../components/DefaultInput';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
+import { useTaskContext } from '../../contexts/TaskContext/useTask';
+import { showMessages } from '../../adapers/show-messages';
 
 export function Settings() {
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    showMessages.dismiss();
+
+    const formErros = [];
+
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput?.current?.value);
+    const longBreakTime = Number(longBreakTimeInput?.current?.value);
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErros.push('Digite apenas valores numéricos para todos os campos');
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErros.push('Digite valores entre 1 e 99 para foco');
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErros.push('Digite valores entre 1 e 30 para descanso curto');
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErros.push('Digite valores entre 1 e 60 para descanso longo');
+    }
+
+    if (formErros.length > 0) {
+      formErros.forEach((error) => showMessages.error(error));
+      return;
+    }
+  }
+
   return (
     <MainTemplate>
       <Container>
@@ -17,15 +56,33 @@ export function Settings() {
         </p>
       </Container>
       <Container>
-        <form action='' className='form'>
+        <form action='' className='form' onSubmit={handleSaveSettings}>
           <div className='formRow'>
-            <DefaultInput id='workTime' labelText='Foco' />
+            <DefaultInput
+              id='workTime'
+              labelText='Foco'
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+              type='number'
+            />
           </div>
           <div className='formRow'>
-            <DefaultInput id='shortBreakTime' labelText='Descanso curto' />
+            <DefaultInput
+              id='shortBreakTime'
+              labelText='Descanso curto'
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+              type='number'
+            />
           </div>
           <div className='formRow'>
-            <DefaultInput id='longBreakTime' labelText='Descanso longo' />
+            <DefaultInput
+              id='longBreakTime'
+              labelText='Descanso longo'
+              ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
+              type='number'
+            />
           </div>
           <div className='formRow'>
             <DefaultButton
