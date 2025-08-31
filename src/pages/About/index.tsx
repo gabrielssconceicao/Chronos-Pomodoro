29d366d791912f763b6fadd3681a03a48081b63d
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-08-31 13:07:54 -0300

    fix: fixing about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -23,8 +23,6 @@ export function About() {
             garantir descansos para evitar o cansaço mental.
           </p>
 
-          <img src='https://placehold.co/1920x1080' alt='' />
-
           <h2>Como funciona o Pomodoro tradicional?</h2>
           <ul>
             <li>
